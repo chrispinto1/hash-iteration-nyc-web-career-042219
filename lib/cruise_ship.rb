6f997cd6e1c53377d61034a0c3d9865c9fd60c2e
Christian Pinto <pintochristian2@gmail.com>
2019-04-18 00:22:00 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,8 @@
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |x|
-     puts x[1].first
+    name = x[1].split('')
+     puts name.first
     if x[0] == "suite_a" && x[1].first == "A"
       return x[1]
     end
